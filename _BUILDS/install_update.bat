d3116b86af73405c55938c2f2a08053951571da4
moelle89 <manuel.moellmann@gmail.com>
2023-12-05 12:08:42 +0100

    fixed install script

--- _BUILDS/install_update.bat
+++ _BUILDS/install_update.bat
@@ -1,39 +1,29 @@
 @echo off
-set "searchTerm=moelles_mojo"
-set "targetDirectories=%APPDATA%\Adobe\After Effects\23\Scripts\ScriptUI Panels
-                      %APPDATA%\Adobe\After Effects\23.1\Scripts\ScriptUI Panels
-                      %APPDATA%\Adobe\After Effects\23.2\Scripts\ScriptUI Panels
-                      %APPDATA%\Adobe\After Effects\23.3\Scripts\ScriptUI Panels
-                      %APPDATA%\Adobe\After Effects\23.4\Scripts\ScriptUI Panels
-                      %APPDATA%\Adobe\After Effects\23.5\Scripts\ScriptUI Panels                      
-					  %APPDATA%\Adobe\After Effects\23.6\Scripts\ScriptUI Panels
-                      %APPDATA%\Adobe\After Effects\23.7\Scripts\ScriptUI Panels                      
-					  %APPDATA%\Adobe\After Effects\23.8\Scripts\ScriptUI Panels                      
-					  %APPDATA%\Adobe\After Effects\23.9\Scripts\ScriptUI Panels"
-set "newFile=moelles_mojo__v0002.jsxbin"
+setlocal enabledelayedexpansion
 
-for %%D in (%targetDirectories%) do (
-    set "targetDirectory=%%D"
+set "searchDir=%APPDATA%\Adobe\After Effects"
+set "targetFolder=Scripts\ScriptUI Panels"
+set "searchString=moelles_mojo"
+set "sourceFile=moelles_mojo.jsxbin"
 
-    echo Searching for files with "%searchTerm%" in the name in "%targetDirectory%"
+for /d %%i in ("%searchDir%\*") do (
+    if exist "%%i\%targetFolder%" (
+        echo Processing files in %%i\%targetFolder%
+        
+        rem Delete files containing the search string
+        for %%f in ("%%i\%targetFolder%\*%searchString%*") do (
+            echo Deleting: %%f
+            del "%%f"
+        )
 
-    for %%F in ("%targetDirectory%\*%searchTerm%*") do (
-        if exist "%%F" (
-            del /q "%%F"
-            echo Deleted: "%%F"
+        rem Copy the specified file to the Script UI Path
+        if exist "%~dp0%sourceFile%" (
+            echo Copying: %sourceFile% to %%i\%targetFolder%
+            copy /Y "%~dp0%sourceFile%" "%%i\%targetFolder%"
         ) else (
-            echo File "%%F" not found.
+            echo Warning: Source file %sourceFile% not found.
         )
     )
-
-    echo Deletion process completed for "%targetDirectory%".
-
-    rem Copy the new file to the current target directory
-    copy "%~dp0%newFile%" "%targetDirectory%"
-
-    echo Copied: "%newFile%" to "%targetDirectory%"
-
-    echo.
 )
 
 pause
