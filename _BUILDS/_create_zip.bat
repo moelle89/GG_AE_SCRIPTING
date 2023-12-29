1ddda33a0f82d7a0c20a79c8df48182ec97771ef
moelle89 <manuel.moellmann@gmail.com>
2023-12-29 01:33:45 +0100

    preparing new build

--- _BUILDS/_create_zip.bat
+++ _BUILDS/_create_zip.bat
@@ -1,14 +1,17 @@
+@echo off
 set ZipOutput="moelles_mojo.zip"
 set File1="install_update.bat"
 set File2="moelles_mojo.jsxbin"
+set SourceFolder="_scripts"
 
 rem Create a temporary directory
 set TempDir="%TEMP%\moelles_mojo"
 mkdir %TempDir%
 
-rem Copy files to the temporary directory
+rem Copy files and folder to the temporary directory
 copy %File1% %TempDir%
 copy %File2% %TempDir%
+xcopy /E /I %SourceFolder% %TempDir%\%SourceFolder%\
 
 rem Zip the files using PowerShell
 powershell -nologo -noprofile -command "& {Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%TempDir%', '%ZipOutput%');}"
