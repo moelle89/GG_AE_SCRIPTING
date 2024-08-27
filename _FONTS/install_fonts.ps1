95357a023150e9033742e1a13cfaf18fb78d6248
moelle <manuel.moellmann@gmail.com>
2024-08-28 00:24:14 +0200

    improved install script

--- _FONTS/install_fonts.ps1
+++ _FONTS/install_fonts.ps1
@@ -7,33 +7,19 @@ New-Item -ItemType Directory -Force -Path $SourceDir
 
 New-Item $TempFolder -Type Directory -Force | Out-Null
 
-# Get all the font files
-$FontFiles = Get-ChildItem -Path $Source -Include '*.ttf','*.ttc','*.otf' -Recurse
+# Get all font files in the source directory
+$fonts = Get-ChildItem -Path $Source -Include '*.ttf','*.ttc','*.otf' -Recurse | Where-Object { -not (Test-Path "C:\Windows\Fonts\$($_.Name)") }
 
-# Check if any new fonts need to be installed
-$NewFonts = $FontFiles | Where-Object { -not(Test-Path "C:\Windows\Fonts\$($_.Name)") }
+# Copy fonts to local temporary folder
+$fonts | ForEach-Object {
+    Copy-Item $_.FullName -Destination $TempFolder -Force
+}
 
-if ($NewFonts.Count -gt 0) {
-    # Copy all new fonts to the temporary folder
-    $NewFonts | ForEach-Object {
-        Copy-Item $_.FullName -Destination $TempFolder
-    }
-
-    # Load the required .NET assemblies
-    [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") | Out-Null
-    [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing.Text") | Out-Null
-
-    # Install the fonts in batches
-    $FontCollection = New-Object System.Drawing.Text.PrivateFontCollection
-    Get-ChildItem -Path $TempFolder | ForEach-Object {
-        $FontCollection.AddFontFile($_.FullName)
-    }
-
-    # Register the fonts with the system
-    $FontCollection.Families | ForEach-Object {
-        [System.Drawing.FontFamily]$_.Name
-    }
-
-    # Clean up the temporary folder
-    Get-ChildItem -Path $TempFolder | Remove-Item -Force
+# Install fonts from temporary folder to Windows Fonts folder without prompting
+$shell = New-Object -ComObject Shell.Application
+$folder = $shell.Namespace(0x14)
+$fonts | ForEach-Object {
+    $font = "$TempFolder\$($_.Name)"
+    $folder.CopyHere($font, 16)  # Use 16 to overwrite without prompting
+    Remove-Item $font -Force
 }
